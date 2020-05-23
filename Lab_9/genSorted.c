4ec45a896c0340b3fc5a669a3dcb50eef0422f0c
Mihalache Ștefan Cristian <57230349+StefanCsPurge@users.noreply.github.com>
2020-05-23 15:42:18 -0500

    Updated thread problem

--- Lab_9/genSorted.c
+++ Lab_9/genSorted.c
@@ -1,3 +1,9 @@
+/*
+Sa se scrie un program care genereaza pe 5 thread-uri distincte 1000 de numere intregi pe care le introduce intr-un vector global. 
+Fiecare numar generat va fi introdus de thread-ul care l-a generat pe pozitia corecta in vector, astfel incat la orice moment vectorul sa fie sortat.
+Threadul principal va afisa la fiecare 10 numere generate intreg continutul vectorului.
+*/
+
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
@@ -5,15 +11,8 @@
 
 #define MAX 99
 
-/*
-Sa se scrie un program care genereaza pe 5 thread-uri distincte 1000 de numere intregi pe care le introduce intr-un vector global. 
-Fiecare numar generat va fi introdus de thread-ul care l-a generat pe pozitia corecta in vector, astfel incat la orice moment vectorul sa fie sortat.
-Threadul principal va afisa la fiecare 10 numere generate intreg continutul vectorului.
-*/
-
 pthread_mutex_t m1 = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t m2 = PTHREAD_MUTEX_INITIALIZER;
-//pthread_cond_t c;
 
 int vector[1000];
 int size;
@@ -45,36 +44,38 @@ void* threadFunction(void* param)
 		else
 			pthread_mutex_unlock(&m2);
 	}
-	pthread_mutex_unlock(&m1);
-	pthread_mutex_unlock(&m2);
 	return NULL;
 }
-
-
 int main()
 {
-
 	srand(time(NULL));
 	pthread_t thr[5];
-	//pthread_cond_init(&c, NULL);
-	for(int i=0;i<5;i++)
+	for(int i=0;i<4;i++)
 		pthread_create(&thr[i],NULL,threadFunction,NULL);
-	
 	while(size<=1000)
 	{
 		pthread_mutex_lock(&m1);
-		for(int i=0;i<size;i++)
-			printf("%d ", vector[i]);
-		printf("\n\n");
+		if(size == 1000)
+		{
+			for(int i=0;i<size;i++)
+				printf("%d ", vector[i]);
+			printf("\n\n");
+			pthread_mutex_unlock(&m2);
+			break;
+		}
+		else
+		{
+			for(int i=0;i<size;i++)
+				printf("%d ",vector[i]);
+			printf("\n\n");
+		}
 		pthread_mutex_unlock(&m2);
 	}
-	pthread_mutex_unlock(&m1);
-	pthread_mutex_unlock(&m2);
-	
-	for(int i=0;i<5;i++)
+	for(int i=0;i<4;i++)
+	{
 		pthread_join(thr[i],NULL);
+	}
 	pthread_mutex_destroy(&m1);
 	pthread_mutex_destroy(&m2);
-	//pthread_cond_destroy(&c);
 	return 0;
 }

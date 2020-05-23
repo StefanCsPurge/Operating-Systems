18efb9a2a384672374fd132d42059cc203709bc7
Mihalache Ștefan Cristian <57230349+StefanCsPurge@users.noreply.github.com>
2020-05-23 12:56:52 -0500

    Updated thread problem

--- Lab_9/genSorted.c
+++ Lab_9/genSorted.c
@@ -3,16 +3,11 @@
 #include <stdlib.h>
 #include <time.h>
 
-/*
-Sa se scrie un program care genereaza pe 5 thread-uri distincte 1000 de numere intregi pe care le introduce intr-un vector global. 
-Fiecare numar generat va fi introdus de thread-ul care l-a generat pe pozitia corecta in vector, astfel incat la orice moment vectorul sa fie sortat.
-Threadul principal va afisa la fiecare 10 numere generate intreg continutul vectorului.
-*/
+#define MAX 99
 
-#define MAX 1000
-
-pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t c;
+pthread_mutex_t m1 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t m2 = PTHREAD_MUTEX_INITIALIZER;
+//pthread_cond_t c;
 
 int vector[1000];
 int size;
@@ -34,12 +29,18 @@ void* threadFunction(void* param)
 	int randNr = rand()%MAX;
 	while(size < 1000)
 	{
-		pthread_mutex_lock(&m);
+		pthread_mutex_lock(&m2);
 		addSorted(randNr);
 		if(size%10 == 0)
-			pthread_cond_signal(&c);
-		pthread_mutex_unlock(&m);
+		{
+			printf("Size %d\n",size);
+			pthread_mutex_unlock(&m1);
+		}
+		else
+			pthread_mutex_unlock(&m2);
 	}
+	pthread_mutex_unlock(&m1);
+	pthread_mutex_unlock(&m2);
 	return NULL;
 }
 
@@ -49,25 +50,25 @@ int main()
 
 	srand(time(NULL));
 	pthread_t thr[5];
-	pthread_cond_init(&c, NULL);
+	//pthread_cond_init(&c, NULL);
 	for(int i=0;i<5;i++)
-	{
 		pthread_create(&thr[i],NULL,threadFunction,NULL);
-	}
 	
-	pthread_mutex_lock(&m);
-	while(size<1000)
+	while(size<=1000)
 	{
-		pthread_cond_wait(&c,&m);
+		pthread_mutex_lock(&m1);
 		for(int i=0;i<size;i++)
 			printf("%d ", vector[i]);
 		printf("\n\n");
+		pthread_mutex_unlock(&m2);
 	}
-	pthread_mutex_unlock(&m);
+	pthread_mutex_unlock(&m1);
+	pthread_mutex_unlock(&m2);
 	
 	for(int i=0;i<5;i++)
 		pthread_join(thr[i],NULL);
-	pthread_mutex_destroy(&m);
-	pthread_cond_destroy(&c);
+	pthread_mutex_destroy(&m1);
+	pthread_mutex_destroy(&m2);
+	//pthread_cond_destroy(&c);
 	return 0;
 }

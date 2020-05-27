e116f22c4a203f896b6018a59af7bdf6503551dc
Mihalache Ștefan Cristian <57230349+StefanCsPurge@users.noreply.github.com>
2020-05-27 17:49:16 +0300

    Add files via upload

---
+++ Lab_7/exAB1.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+
+void processB()
+{
+	int d = open("myfifo",O_RDONLY);
+	int n,x,nr=0,sum=0;
+	if(read(d,&n,sizeof(int)) < 0)
+	{
+		perror("Read failed!");
+		exit(1);
+	}
+	while(read(d,&x,sizeof(int)))
+	{
+		if(n%x == 0)
+		{
+			nr++;
+			sum += x;
+		}
+	}
+	
+	printf("In B: %d numbers are div with %d\n",nr,n);
+	
+	d = open("myfifo",O_WRONLY);
+	if (write(d,&sum,sizeof(int)) < 0)
+	{
+		perror("Write failed!");
+		exit(1);
+	}
+	close(d);
+	exit(0);
+}
+
+int main()
+{
+	mkfifo("myfifo",0600);
+	if(fork() == 0)
+		processB();
+	
+	//process A
+	int d = open("myfifo",O_WRONLY);
+
+	int n, x;
+	printf("N = ");
+	scanf("%d",&n);
+	write(d,&n,sizeof(int));
+
+	printf("Enter number: ");
+	scanf("%d",&x);
+	while(x)
+	{
+		write(d,&x,sizeof(int));
+		printf("Enter number: ");
+		scanf("%d",&x);
+	}
+	close(d);
+	wait(0);
+
+	d = open("myfifo",O_RDONLY);
+	int sum = 0;
+	if(read(d,&sum, sizeof(int)) < 0)
+	{
+		perror("Read failed!");
+		exit(1);
+	}
+	printf("The sum is %d\n",sum);
+	
+	close(d);
+	unlink("myfifo");
+	return 0;
+}
+
+
+
+
+
